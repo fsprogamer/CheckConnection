90d9371e04a336e6e740815ae46170e7a1c546fb
fsprogamer <fsprogamer@gmail.com>
2016-09-13 17:43:54 +0300

    and now ... interface

---
+++ CheckConnection/Methods/DBInterface.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using CheckConnection.Model;
+using SQLite;
+
+namespace CheckConnection.Methods
+{
+    public interface DBInterface
+    {
+        void SaveConnectionTable(List<Connection> Connection_list,
+                                 List<DNS> DNS_list,
+                                 List<Gateway> Gateway_list
+                                    );
+        void SaveDNSTable(List<DNS> DNS_list, SQLiteConnection db, int connId);
+
+        void SaveGatewayTable(List<Gateway> Gateway_list, SQLiteConnection db, int connId);
+
+        List<Connection> ReadConnectionHistory();
+
+        List<DNS> ReadDNSHistory(int Connection_Id);
+
+        List<Gateway> ReadGatewayHistory(int Connection_Id);
+
+        bool isTableExists(String tableName, SQLiteConnection db);
+
+    }
+}
---
+++ CheckConnection/Methods/WMIInterface.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using CheckConnection.Model;
+
+namespace CheckConnection.Methods
+{
+    public interface WMIInterface
+    {
+        List<Connection> GetNetworkDevices();
+
+        List<DNS> GetDNSArray(int Connection_Id);
+
+        List<Gateway> GetGatewayArray(int Connection_Id);
+
+        List<Ping> GetPingResult(string PingAddress);
+    }
+}
